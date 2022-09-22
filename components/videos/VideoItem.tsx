a3538f98b11a3d24ea2b107c095867360e367a7d
M4p4 <homejr@protonmail.com>
2022-09-22 19:10:50 +0200

    wip

--- components/videos/VideoItem.tsx
+++ components/videos/VideoItem.tsx
@@ -26,7 +26,7 @@ const VideoItem: FC<Props> = ({
         <div className="bg-white hover:bg-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700 text-gray-600 hover:text-gray-500 dark:hover:text-gray-100 dark:text-gray-300 justify-items-center rounded-md overflow-hidden hover:scale-[1.05] duration-300 shadow-lg group">
           <div className="relative">
             <img
-              className="w-fill object-fill"
+              className="w-full object-fill"
               alt={video.title}
               src={video.image}
             />
