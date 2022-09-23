33dcc85621495e1c8aabcdc8565302e57718ce9c
M4p4 <homejr@protonmail.com>
2022-09-23 14:41:42 +0200

    outline + refactor headline

--- components/ui/Headline.tsx
+++ components/ui/Headline.tsx
@@ -1,32 +1,42 @@
 import React, { FC } from 'react';
+import { classNames } from 'utils/helpers';
 
 type Props = {
   variant?: string;
+  outline?: boolean;
   text: string;
 };
 
-const Headline: FC<Props> = ({ text, variant = 'h1' }) => {
+const getTextSize = (variant: string) => {
+  switch (variant) {
+    case 'h3':
+      return 'text-xl md:text-2xl';
+    case 'h2':
+      return 'text-2xl md:text-3xl';
+    case 'h1':
+    default:
+      return 'text-3xl md:text-4xl';
+  }
+};
+
+const Headline: FC<Props> = ({ text, variant = 'h1', outline = false }) => {
+  let className = classNames(
+    'mb-1 md:mb-3  font-semibold',
+    getTextSize(variant),
+    outline
+      ? 'bg-indigo-700 text-slate-100 dark:text-gray-100 dark:bg-slate-800/80 rounded-md p-2 text-center md:text-left'
+      : 'text-slate-800 dark:text-gray-100'
+  );
+
   if (variant == 'h3') {
-    return (
-      <h3 className="text-md md:text-2xl mb-1 md:mb-3 text-slate-800 dark:text-gray-100  font-semibold">
-        {text}
-      </h3>
-    );
+    return <h3 className={className}>{text}</h3>;
   }
 
   if (variant == 'h2') {
-    return (
-      <h2 className="text-xl md:text-3xl mb-1 md:mb-3 text-slate-800 dark:text-gray-100 font-semibold">
-        {text}
-      </h2>
-    );
+    return <h2 className={className}>{text}</h2>;
   }
 
-  return (
-    <h1 className="text-2xl md:text-4xl mb-1 md:mb-3 text-slate-800 dark:text-gray-100 font-semibold">
-      {text}
-    </h1>
-  );
+  return <h1 className={className}>{text}</h1>;
 };
 
 export default Headline;
