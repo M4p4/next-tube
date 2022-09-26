4cb6324e8eec1bd1d3361575bb5ef0db9d1cd9b4
M4p4 <homejr@protonmail.com>
2022-09-26 11:31:24 +0200

    utils functions

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,17 @@
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
+
+export const isEmpty = (val: any) => {
+  return val === undefined || val == null || val.length <= 0 ? true : false;
+};
+
+export const slugify = (str: string) => {
+  str = str
+    .trim()
+    .toLowerCase()
+    .replaceAll(' ', '-')
+    .replaceAll('.', '-')
+    .replace(/-+/g, '-');
+  return str;
+};
