251dfc5e84ca4616f52f161670b56740cdeb2fb8
M4p4 <homejr@protonmail.com>
2022-10-07 08:14:24 +0200

    added quick navigation to statistic

--- components/panel/overview/OverviewItem.tsx
+++ components/panel/overview/OverviewItem.tsx
@@ -5,11 +5,13 @@ import {
   VideoCameraIcon,
   ViewGridIcon,
 } from '@heroicons/react/outline';
-import React, { FC } from 'react';
+import Link from 'next/link';
+import React, { FC, HTMLAttributeReferrerPolicy } from 'react';
 
 type Props = {
   title: string;
   count: number;
+  href: string;
 };
 
 const getIcon = (icon: string) => {
@@ -35,15 +37,21 @@ const getIcon = (icon: string) => {
   );
 };
 
-const OverviewItem: FC<Props> = ({ title, count }) => {
+const OverviewItem: FC<Props> = ({ title, count, href }) => {
   return (
-    <div className="bg-slate-800 text-gray-100 p-3 rounded-md w-full flex flex-row justify-between items-center border-b-4 border-slate-700 group">
-      <div className="rounded-full p-2 bg-slate-100">{getIcon(title)}</div>
-      <div className="flex flex-col items-center justify-center w-24 md:w-auto">
-        <div className="text-2xl font-semibold text-indigo-500">{count}</div>
-        <div className="font-semibold text-base">{title}</div>
-      </div>
-    </div>
+    <Link href={href}>
+      <a className="w-full">
+        <div className="bg-slate-800 text-gray-100 p-3 rounded-md w-full flex flex-row justify-between items-center border-b-4 border-slate-700 group">
+          <div className="rounded-full p-2 bg-slate-100">{getIcon(title)}</div>
+          <div className="flex flex-col items-center justify-center w-24 md:w-auto">
+            <div className="text-2xl font-semibold text-indigo-500">
+              {count}
+            </div>
+            <div className="font-semibold text-base">{title}</div>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
--- components/panel/overview/index.tsx
+++ components/panel/overview/index.tsx
@@ -13,10 +13,26 @@ type Props = {
 const OverviewSection: FC<Props> = ({ counts }) => {
   return (
     <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:space-x-2">
-      <OverviewItem title="Videos" count={counts.videosCount} />
-      <OverviewItem title="Tags" count={counts.tagsCount} />
-      <OverviewItem title="Categories" count={counts.categoriesCount} />
-      <OverviewItem title="Actors" count={counts.actorsCount} />
+      <OverviewItem
+        title="Videos"
+        href="/panel/videos"
+        count={counts.videosCount}
+      />
+      <OverviewItem
+        title="Tags"
+        count={counts.tagsCount}
+        href="/panel/tags?role=tag"
+      />
+      <OverviewItem
+        title="Categories"
+        count={counts.categoriesCount}
+        href="/panel/tags?role=category"
+      />
+      <OverviewItem
+        title="Actors"
+        count={counts.actorsCount}
+        href="/panel/tags?role=actor"
+      />
     </div>
   );
 };
