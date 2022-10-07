85bde2b13b2385e4b56c735b5a720f2ec32c97dd
M4p4 <homejr@protonmail.com>
2022-10-07 08:40:17 +0200

    refactor api

---
+++ pages/api/tags/index.ts
@@ -0,0 +1,19 @@
+import { connectToDbHandler } from 'database/database';
+import { addTag } from 'database/services/tags.service';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === 'POST') {
+      const tag = await addTag(req);
+      return res.status(200).send(tag);
+    }
+  } catch (err: any) {
+    return res.status(400).json({
+      message: `API Error ${err.message || 'TAGS/ADD unknown error'}`,
+    });
+  }
+  return res.status(400).json({ message: 'API Error' });
+};
+
+export default connectToDbHandler(handler);
