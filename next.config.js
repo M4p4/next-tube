71ba89f09e4fe18ed1b97f261d5de46bb31d5431
M4p4 <homejr@protonmail.com>
2022-10-10 16:00:01 +0200

    added analyse tool

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
