0843386667a99396d6f4769351c2c859b2933c07
M4p4 <homejr@protonmail.com>
2022-10-10 19:01:26 +0200

    added image to tag edit modal

--- components/panel/modals/EditTagModal.tsx
+++ components/panel/modals/EditTagModal.tsx
@@ -1,3 +1,4 @@
+import { RefreshIcon } from '@heroicons/react/outline';
 import LabelCheckbox from '@panel/ui/Checkbox';
 import DropDown from '@panel/ui/Dropdown';
 import PanelModal from '@panel/ui/Modal';
@@ -21,39 +22,58 @@ const EditTagModal: FC<Props> = ({ isShowing, onClose, id, saveChanges }) => {
     <PanelModal isShowing={isShowing} title="Edit Tag" onClose={onClose}>
       {tag ? (
         <>
-          <PanelTextInput
-            label="Name"
-            value={tag.name}
-            handleChange={(e) => {
-              updateTag('name', e.target.value);
-            }}
-          />
+          <div className="flex flex-col md:flex-row justify-between md:space-x-4">
+            <div className="flex flex-col w-full">
+              <PanelTextInput
+                label="Name"
+                value={tag.name}
+                handleChange={(e) => {
+                  updateTag('name', e.target.value);
+                }}
+              />
 
-          <LabelCheckbox
-            label="Priority"
-            value={tag.priority}
-            handleChange={() => {
-              updateTag('priority', !tag.priority);
-            }}
-          />
+              <LabelCheckbox
+                label="Priority"
+                value={tag.priority}
+                handleChange={() => {
+                  updateTag('priority', !tag.priority);
+                }}
+              />
 
-          <PanelTextInput
-            label="Video Count"
-            value={tag.videoCount}
-            handleChange={(e) => {
-              updateTag('videoCount', e.target.value);
-            }}
-          />
+              <PanelTextInput
+                label="Video Count"
+                value={tag.videoCount}
+                handleChange={(e) => {
+                  updateTag('videoCount', e.target.value);
+                }}
+              />
 
-          <div className="flex flex-col">
-            <span className="font-semibold my-1">Role</span>
-            <DropDown
-              items={TAG_ROLES_DROPDOWN}
-              selectedQuery={tag.role}
-              updateFilterQuery={(newRole) => {
-                updateTag('role', newRole as string);
-              }}
-            />
+              <div className="flex flex-col">
+                <span className="font-semibold my-1">Role</span>
+                <DropDown
+                  items={TAG_ROLES_DROPDOWN}
+                  selectedQuery={tag.role}
+                  updateFilterQuery={(newRole) => {
+                    updateTag('role', newRole as string);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col min-w-fit justify-center">
+              <span className="font-semibold my-1">Image</span>
+              <img
+                className="w-48 h-48 md:rounded-t-md rounded-md md:rounded-b-none mx-auto"
+                src={tag.image || '/images/no-image.png'}
+              />
+              <div>
+                <button className="bg-sky-700 hover:bg-sky-600 p-1 md:rounded-b-md shadow-lg w-full mt-2 md:mt-0 rounded-md md:rounded-t-none">
+                  <div className="flex justify-center items-center">
+                    <RefreshIcon className="w-5 h-5" />
+                  </div>
+                </button>
+              </div>
+            </div>
           </div>
 
           <div className="flex flex-col md:flex-row md:space-y-0 w-full space-y-2 md:space-x-2 mt-8">
