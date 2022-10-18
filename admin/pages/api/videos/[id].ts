73f5e56130f642130540d79cc18879f3b021966a
M4p4 <homejr@protonmail.com>
2022-10-18 15:15:49 +0200

    video edit / delete updates now image

--- admin/pages/api/videos/[id].ts
+++ admin/pages/api/videos/[id].ts
@@ -5,7 +5,9 @@ import {
   updateVideo,
 } from '@db/services/videos.service';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { VideoWithMeta } from 'types/types';
 import { hasSession } from 'utils/auth';
+import { createImage, deleteImage } from 'utils/cdn';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -19,11 +21,47 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(200).json(video);
     }
     if (req.method === 'PATCH') {
-      const tag = await updateVideo(+id, req.body);
-      return res.status(200).send({ tag, message: 'Video updated' });
+      const video = (await getVideoById(+id)) as VideoWithMeta;
+
+      let updatedVideo = null;
+
+      if (
+        video.title !== req.body.title ||
+        video.originalImage !== req.body.originalImage
+      ) {
+        deleteImage(video.originalImage, video.title, '', '_thumb');
+        deleteImage(video.originalImage, video.title, '', '_poster');
+        const thumbnail = await createImage(
+          req.body.originalImage,
+          req.body.title,
+          168,
+          300,
+          '',
+          '_thumb'
+        );
+        const poster = await createImage(
+          req.body.originalImage,
+          req.body.title,
+          280,
+          500,
+          '',
+          '_poster'
+        );
+        updatedVideo = await updateVideo(+id, {
+          ...req.body,
+          thumbnail: thumbnail,
+          poster: poster,
+        });
+      } else {
+        updatedVideo = await updateVideo(+id, req.body);
+      }
+      return res.status(200).send({ updatedVideo, message: 'Video updated' });
     }
     if (req.method === 'DELETE') {
-      const tag = await removeVideoById(+id);
+      const video = (await getVideoById(+id)) as VideoWithMeta;
+      deleteImage(video.originalImage, video.title, '', '_thumb');
+      deleteImage(video.originalImage, video.title, '', '_poster');
+      await removeVideoById(+id);
       return res.status(200).send({ message: 'Video deleted' });
     }
   } catch (err: any) {
