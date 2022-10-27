154977597d9a60ff5a638b6a83d763cc3725db09
M4p4 <homejr@protonmail.com>
2022-10-27 18:06:51 +0200

    added Link to VideoCategories Component

--- tube/components/video/VideoCategories.tsx
+++ tube/components/video/VideoCategories.tsx
@@ -1,5 +1,7 @@
 import { UserCircleIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
 import React, { FC } from 'react';
+import { buildTagUrl } from 'utils/navigation';
 
 type Props = {
   categories: string[];
@@ -10,20 +12,22 @@ const VideoCategories: FC<Props> = ({ categories, models }) => {
   return (
     <div className="flex flex-row mt-2 items-center justify-start flex-wrap overflow-hidden mb-2 md:mb-0">
       {categories.map((category) => (
-        <div
-          key={category}
-          className="py-1 px-3 border-2 border-secondary hover:bg-tertiary rounded-md cursor-pointer mr-2 mb-2 inline-flex items-center"
-        >
-          <UserCircleIcon className="w-5 h-5 mr-1" /> {category}
-        </div>
+        <Link key={category} href={buildTagUrl(category, 'category')}>
+          <a>
+            <div className="py-1 px-3 border-2 border-secondary hover:bg-tertiary rounded-md cursor-pointer mr-2 mb-2 inline-flex items-center">
+              <UserCircleIcon className="w-5 h-5 mr-1" /> {category}
+            </div>
+          </a>
+        </Link>
       ))}
       {models.map((model) => (
-        <div
-          key={model}
-          className="py-1 px-3 border-2 border-secondary hover:bg-tertiary rounded-md cursor-pointer mr-2 mb-2 inline-flex items-center"
-        >
-          <UserCircleIcon className="w-5 h-5 mr-1" /> {model}
-        </div>
+        <Link key={model} href={buildTagUrl(model, 'model')}>
+          <a>
+            <div className="py-1 px-3 border-2 border-secondary hover:bg-tertiary rounded-md cursor-pointer mr-2 mb-2 inline-flex items-center">
+              <UserCircleIcon className="w-5 h-5 mr-1" /> {model}
+            </div>
+          </a>
+        </Link>
       ))}
     </div>
   );
